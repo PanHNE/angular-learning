aafdb9adedae250a150e0f908bdf85d9629c8b0f
Adrian Domin <adriandominx@gmail.com>
2024-01-05 13:00:29 +0100

    Get form data to component and show them in console

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('form') signupForm: NgForm;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log("onSubmit() => ... ");
+  //   console.log(form);
+  //   console.log(form.value);
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm);
   }
 }
