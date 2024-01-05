0452fc9e6e87a5ec85639ae8568c52a943c1295e
Adrian Domin <adriandominx@gmail.com>
2024-01-05 15:08:06 +0100

    Finish creating basic form

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,8 +9,40 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('form') signupForm: NgForm;
 
+  defaultQuestion = "pet";
+  answer = "";
+  genders = ["female", "male"];
+
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    
+    // to set all inputs in your form
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionArea: '',
+    //   gender: 'male'
+    // });
+
+    // to set part inputs in your form
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
@@ -21,5 +53,13 @@ export class AppComponent {
 
   onSubmit() {
     console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
