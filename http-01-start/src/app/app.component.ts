d14571e6dad1f9c9dc2130b443798acea44d3d3b
Adrian Domin <adriandominx@gmail.com>
2024-01-08 13:48:44 +0100

    Add sending data to server and getting data from there

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -13,16 +15,43 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {}
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     console.log(postData);
+    this.http.post<{ name: string }>(
+      'https://learning-b5a0b-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+      postData
+    ).subscribe( response => {
+      console.log(response);
+    });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts(){
+    this.http.get<{ [key: string]: Post }>('https://learning-b5a0b-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+      .pipe(
+        map( responseData => {
+          const postsArray: Post[] = [];
+
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key});
+            }
+          }
+
+          return postsArray;
+      }))
+      .subscribe( posts => {
+        console.log(posts);
+        this.loadedPosts = posts;
+      });
   }
 }
